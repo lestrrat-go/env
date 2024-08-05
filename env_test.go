39a659f5eda9dedd2ef3f386c84c61bffa3d7e59
Daisuke Maki <lestrrat+github@gmail.com>
2024-08-05 10:18:38 +0900

    tweak test

--- env_test.go
+++ env_test.go
@@ -9,9 +9,12 @@ import (
 )
 
 func TestEnv(t *testing.T) {
+	require.Equal(t, "MYAPP_FOO", env.VarName("FOO"))
 	env.SetPrefix("LESTRRAT_GO_ENV")
 	os.Setenv("LESTRRAT_GO_ENV_FOO", "foo")
 	require.Equal(t, "foo", env.Value("FOO", ""))
 	env.SetValue("BAR", "bar")
 	require.Equal(t, "bar", env.Value("BAR", ""))
+	env.SetPrefix("")
+	require.Equal(t, "MYAPP_FOO", env.VarName("FOO"))
 }
